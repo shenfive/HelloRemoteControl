ecd2da8d17b9f369fff54e60283e64a4b56740fb
Shen Jun Wu <shenfive@gmail.com>
2022-07-18 11:44:38 +0800

    Initial Commit

---
+++ HelloRemoteControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloRemoteControl
+//
+//  Created by 申潤五 on 2022/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
